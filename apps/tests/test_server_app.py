7e7a69f065540a3b8f257ef5c0d6113393f1a85c
Marcus Vinicius Braga <marvinbraga@users.noreply.github.com>
2021-09-02 00:40:12 -0300

    Actions Test Adjust

--- apps/tests/test_server_app.py
+++ apps/tests/test_server_app.py
@@ -9,10 +9,25 @@ Aug 2021
 Test Server Application Module
 """
 from multiprocessing.connection import Client
+from time import sleep
 
 import pytest
 
 from core import settings
+from main import MainApplication
+
+main_app = MainApplication()
+
+
+@pytest.fixture
+def initialize_mock_main():
+    """
+    Start Instance of Main Class with mock server.
+    :return: Object or False
+    """
+    global main_app
+    result = main_app.start('zap_server_app.py', '--mock') if main_app else False
+    return result
 
 
 @pytest.fixture
@@ -21,14 +36,16 @@ def initialize_client_app():
     Start Instance of client to ZapServer.
     :return: Object or False
     """
+    sleep(5)
     return Client(('0.0.0.0', 8777), authkey=settings.AUTH_KEY.encode())
 
 
-def test_start_main(initialize_client_app):
+def test_start_main(initialize_mock_main, initialize_client_app):
     """
     Check Zap Server Initialization.
     :return:
     """
+    s = initialize_mock_main
     c = initialize_client_app
     command = 'test_start_main||SendMessage||Contact Name||Initial Test.'
     c.send(command)
